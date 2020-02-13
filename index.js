c76dfa0f588ec4a7eb00fb3d7b83c466d7747ef7
melenJD <el.todos17@gmail.com>
2020-02-13 16:40:17 +0900

    add static folder

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res){
-  res.send('hello world');
-})
+app.use(express.static(__dirname + '/public'));
 
 var port = 3000;
 app.listen(port, function(){
