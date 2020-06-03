acd1eb1443211f3b9b85f9bfa28ae18ab86e2441
Jidong Kim <el.todos17@gmail.com>
2020-06-03 23:10:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/hello/:nameParam', function(req, res){
   res.render('hello', {name:req.params.nameParam});
 });
 
-var port = 80;
+var port = 3000;
 app.listen(port, function(){
   console.log('Server Open');
 });
